0f87f567f944aaa2f3938cf1a5269782bd161fee
josoomin <99530921+josoomin@users.noreply.github.com>
2022-08-18 15:44:39 +0900

    22.08.18

---
+++ Assets/escape room/Scripts/BookShelves.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace roomescape
+{
+    public class BookShelves : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        private void OnMouseDown()
+        {
+            Debug.Log("책상 터치");
+        }
+    }
+}
